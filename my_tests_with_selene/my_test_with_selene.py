d6e13661de80081109e9ecf3dd6214930245a82f
cshimm <cameronjshimmin@gmail.com>
2024-04-20 21:04:53 -0700

    update path to use project version of selene

--- my_tests_with_selene/my_test_with_selene.py
+++ my_tests_with_selene/my_test_with_selene.py
@@ -1,6 +1,13 @@
+import sys
+import os
+
+# Find the absolute path of the directory containing your module
+module_dir = os.path.relpath(r'selene')
+# Add the absolute path to sys.path
+sys.path.append(module_dir)
 from selene import browser, by, be, have
 browser.open('https://google.com/ncr')
 browser.element(by.name('q')).should(be.blank) \
     .type('selenium').press_enter()
-browser.all('#rso>div').should(have.size_greater_than(5)) \
+browser.all('#rso>div').should(have.size_greater_than(1)) \
     .first.should(have.text('Selenium automates browsers'))
